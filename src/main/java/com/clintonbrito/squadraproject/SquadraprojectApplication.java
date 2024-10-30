beb2712223a75416185e6eee4bb3e29f1dd2b015
Clinton Brito <clintoncsb@gmail.com>
2024-10-30 18:35:12 -0300

    feat: start a new project

---
+++ src/main/java/com/clintonbrito/squadraproject/SquadraprojectApplication.java
@@ -0,0 +1,13 @@
+package com.clintonbrito.squadraproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadraprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadraprojectApplication.class, args);
+	}
+
+}
